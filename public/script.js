8f3d0585be1ceb739128f981933c32f8c0766247
vandat696 <datthitran2005@gmail.com>
2025-01-09 14:02:17 +0700

    a

--- public/script.js
+++ public/script.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const awayPredictionInput = document.getElementById('away-prediction');
   const placeBetButton = document.getElementById('place-bet');
 
-  let homeTeamName = 'Wolverhampton Wanderers FC'; 
-  let awayTeamName = 'Nottingham Forest FC';
+  let homeTeamName = 'Fulham FC'; 
+  let awayTeamName = 'Watford FC';
   let matchStatus = 'TIMED'; 
   let points = 100000; 
   let userId = localStorage.getItem('userId');
