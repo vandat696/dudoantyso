3acc2d5946825fa63c5b0e4e9fde4e0333f32fff
vandat696 <datthitran2005@gmail.com>
2025-01-09 14:35:41 +0700

    test

--- public/script.js
+++ public/script.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const awayPredictionInput = document.getElementById('away-prediction');
   const placeBetButton = document.getElementById('place-bet');
 
-  let homeTeamName = 'Fulham FC'; 
-  let awayTeamName = 'Watford FC';
+  let homeTeamName = 'Real Madrid CF'; 
+  let awayTeamName = 'RCD Mallorca';
   let matchStatus = 'TIMED'; 
   let points = 100000; 
   let userId = localStorage.getItem('userId');
